90f9ce43a0dbff581e00079876ac82dc057a3948
Kirill <ivanteev87@mail.ru>
2022-05-10 01:56:07 +0300

    program work

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+System.Console.WriteLine();
+
+int number = 2;
+int exponent = 4;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
